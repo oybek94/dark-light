982104c83f2f3f4d33ba413c12e264bbddfad3c7
oybek94 <oybek94@bk.ru>
2021-04-03 09:22:32 +0500

    bugs fixed

--- js/filter.js
+++ js/filter.js
@@ -1,90 +1,21 @@
-// Initiate Muuri Grid
-const grid = new Muuri('.grid');
-console.log("hello")
-
-// Hold our selected filters
-let filteredCategories = [];
-
-// Get All Filterable values
-const categoryFilter = document.getElementById('category-filter');
-const allCategories = Array.from(categoryFilter.querySelectorAll('option'));
-
-// Set Defaults
-let selectedCategory = categoryFilter.value;
-
-
-// Events ------------------------------------------------------
-// filter categories on select
-const selects = document.querySelectorAll('select');
-selects.forEach( item => {
-	item.addEventListener('change', () => {
-		filterGrid();
-	}); 
+//Filter Recruiters
+$('select#category-filter').change(function () {
+	var filter = $(this).val();
+	filterList(filter);
 });
 
-
-
-// Filter grid ------------------------------------------------
-function filterGrid() {
-	
-	// Get latest select values
-	selectedCategory = categoryFilter.value;
-	
-	// Reset filtered categories 
-	filteredCategories.splice(0,filteredCategories.length);
-	
-	// Categories
-	if( selectedCategory == 'all' ) {
-		 allCategories.forEach( (item) => {
-			 // exlude the actual 'all' value
-			( item.value !="all" ? filteredCategories.push(item.value) : '' );
+// Recruiters filter function
+function filterList(value) {
+	var list = $(".cards .card");
+	$(list).hide();
+	if (value == "all") {
+		$(".cards").find("article").each(function (i) {
+			$(this).show();
 		});
 	} else {
-		filteredCategories.push(categoryFilter.value);
+		// *=" means that if a data-custom type contains multiple values, it will find them
+		$(".cards").find("article[data-category*=" + value + "]").each(function (i) {
+			$(this).show();
+		});
 	}
-	console.table(filteredCategories);
-	
-	
-	// Filter the grid
-	// For each item in the grid array (which corresponds to a gallery item), check if the data-categories and data-types value match any of the values in the select field. If they do, return true
-	grid.filter( (item) => {
-		if (
-				filteredCategories.some( (cat) => {
-					return (item.getElement().dataset.category).indexOf(cat) >= 0;
-				})
-			&& // set this to 'or' if you want either option to be true. This way both must be true for the item to display
-			filteredTypes.some( (typ) => {
-					return (item.getElement().dataset.type).indexOf(typ) >= 0;
-				})
-			
-		) {
-			// return items that match both IFs
-			return true;
-		}
-		
-	});
-} // end filter grid function
-
-
-
-// Slim Select ---------------
-new SlimSelect({
-	select: '#category-filter',
-	showSearch: false,
-});
-
-new SlimSelect({
-	select: '#type-filter',
-	showSearch: false,
-});
-
-
-// Refresh gallery item dimensions and do muuri refresh after last image has loaded (otherwise delayed loading of image will throw off muuri child element sizing, and items will overlap).
-const galleryImages = document.querySelectorAll('.image-gallery__item img');
-let galleryImagesCount = 0;
-galleryImages.forEach( (item) => {
-	item.addEventListener('load', function () {
-  galleryImagesCount++;
-		(galleryImagesCount == galleryImages.length ?grid.refreshItems().layout() : '' );
-	});
-});
+}
